6466a19cf8cc18ac36c1524d3be01fdeacb3ccad
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2021-08-12 19:11:34 -0300

    Teste Security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,8 @@ INSERT INTO BRAND (name) VALUES ('Ferrari');
 INSERT INTO VEHICLE(model, year, price, brand_id) VALUES ("onix", "2022", "100000", 1);
 INSERT INTO VEHICLE(model, year, price, brand_id) VALUES ("Kwid", "2018", "50000", 2);
 INSERT INTO VEHICLE(model, year, price, brand_id) VALUES ("Opala", "2000", "30000", 3);
+
+INSERT INTO TB_USUARIO (email, name, senha) VALUES ('brunoviniciustica@gmail.com', 'Bruno Vinicius Felix Da Silva', '$2a$10$za0OmFoDGW6A.YZM8zP4JOjypne6wQMR/7xgWEBMkyKwoLLyPQRvm');
+INSERT INTO TB_USUARIO (email, nome, senha) VALUES ('test@gmail.com', 'Teste da Silva Junior', '$2a$10$Gn03nA57itFW1RqrQCOZPON5b2L296pX0HBcVwpuHiyyD7qBpph/C');
+INSERT INTO TB_USUARIO (email, nome, senha) VALUES ('nomeTest@gmail.com', 'Nome Teste 01', '$2a$10$Gn03nA57itFW1RqrQCOZPON5b2L296pX0HBcVwpuHiyyD7qBpph/C');
+INSERT INTO TB_USUARIO (email, nome, senha) VALUES ('furustreca@gmail.com', 'Furustreca Lima', '$2a$10$Gn03nA57itFW1RqrQCOZPON5b2L296pX0HBcVwpuHiyyD7qBpph/C');
